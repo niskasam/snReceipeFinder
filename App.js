37280c7944a93e777eab42552bd8ec8fe070bd99
niskasam <samnisk@outlook.com>
2021-11-09 23:13:08 +0200

    snReceipeFinder, 9.11.2021

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, Alert, Image, Pressable} from 'react-native';
 
 export default function App() {
+
+  const [keyword, setKeyword] = useState('');
+  const [meals, setMeals] = useState([]);
+
+  const getMeals = () => {fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+  .then(response => response.json())
+  .then( responseJSON => {
+    setMeals(responseJSON.meals)})
+  .catch(error => { Alert.alert('Error', error); }); 
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <TextInput
+    style={styles.input}
+    placeholder='Search meals by ingredient'
+    onChangeText={ text=> setKeyword(text) } />
+
+     <Pressable
+     style={styles.btnContainer}
+     onPress={getMeals}>
+     <Text
+     style={styles.btn}>Find meal</Text>
+     </Pressable>
+
+     <View style={styles.listContainer}>
+     <FlatList
+     style={styles.list}
+     keyExtractor={(item,  index) => index.toString()}
+     data={meals}
+     renderItem={({item}) =>
+     <View>
+       <Text style={styles.text}>{item.strMeal}</Text>
+       <Text><Image
+          style={styles.image}
+          source={{uri:`${item.strMealThumb}/preview`}}
+              // source = {{uri: 'https://www.themealdb.com//images/media/meals/llcbn01574260722.jpg/preview'}}
+          /></Text>
+     </View>
+      }
+     
+     />
+     </View>
+
     </View>
   );
 }
@@ -15,7 +56,48 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:80,
+    
+    
   },
+  image: {
+    width: 100,
+    height: 60,
+  },
+  input:{
+    textAlign:'center',
+    height: "8%",
+  },
+
+  btnContainer:{
+    alignItems:'center',
+    justifyContent:'center',
+  },
+
+  btn:{
+    color: 'white',
+    width: "80%",
+    backgroundColor: 'green',
+    textAlign:'center',
+    height: 50,
+    borderRadius: 10,
+    padding: 10,
+    fontSize: 18,
+  },
+
+  listContainer:{
+    marginLeft: 10,
+    flex: 2,
+  },
+
+  list:{
+    marginTop: 30,
+  },
+
+  text:{
+    fontSize: 16,
+    fontWeight: 'bold',
+  }
+
 });
+
